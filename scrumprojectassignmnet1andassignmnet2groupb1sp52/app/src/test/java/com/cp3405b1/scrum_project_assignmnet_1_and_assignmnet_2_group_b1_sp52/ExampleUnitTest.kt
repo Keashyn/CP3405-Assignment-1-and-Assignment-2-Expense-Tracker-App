a996fef46bb71502940449d5f8aed91bc56c275e
Keashyn <68360382+Keashyn@users.noreply.github.com>
2021-08-12 16:07:19 +0800

    Add files via upload

---
+++ scrumprojectassignmnet1andassignmnet2groupb1sp52/app/src/test/java/com/cp3405b1/scrum_project_assignmnet_1_and_assignmnet_2_group_b1_sp52/ExampleUnitTest.kt
@@ -0,0 +1,17 @@
+package com.cp3405b1.scrum_project_assignmnet_1_and_assignmnet_2_group_b1_sp52
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+class ExampleUnitTest {
+    @Test
+    fun addition_isCorrect() {
+        assertEquals(4, 2 + 2)
+    }
+}
