8d8813a1bf51b39555de4634b33b687d4cf0dfc5
ClementSeah <69644707+ClementSeah@users.noreply.github.com>
2021-08-16 21:00:53 +0800

    Add Query for Database

--- app/src/main/java/com/cp3405b1/expensetrackerapp/db/TransactionDao.kt
+++ app/src/main/java/com/cp3405b1/expensetrackerapp/db/TransactionDao.kt
@@ -2,6 +2,7 @@ package com.cp3405b1.expensetrackerapp.db
 
 import androidx.room.*
 import com.cp3405b1.expensetrackerapp.model.Transaction
+import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface TransactionDao {
@@ -15,4 +16,15 @@ interface TransactionDao {
     @Delete
     suspend fun deleteTransaction(transaction: Transaction)
 
+    @Query("SELECT * FROM all_transactions ORDER by createdAt DESC")
+    fun getAllTransaction() : Flow<List<Transaction>>
+
+    @Query("SELECT * FROM all_transactions WHERE transactionType == :transactionType ORDER by createdAt DESC")
+    fun getTransaction(transactionType:String) : Flow<List<Transaction>>
+
+    @Query("SELECT * FROM all_transactions WHERE id = :id")
+    fun getTransactionByID(id: Int): Flow<androidx.room.Transaction>
+
+    @Query("DELETE FROM all_transactions WHERE id = :id")
+    suspend fun deleteTransactionByID(id: Int)
 }
