7a2406d0c47325b125e5eb6b448733ab4ccc526b
Keashyn <68360382+Keashyn@users.noreply.github.com>
2021-08-23 16:09:27 +0800

    edit these files

--- app/src/main/java/com/cp3405b1/expensetrackerapp/view/about/AboutViewModel.kt
+++ app/src/main/java/com/cp3405b1/expensetrackerapp/view/about/AboutViewModel.kt
@@ -7,14 +7,14 @@ import kotlinx.coroutines.flow.StateFlow
 
 @HiltViewModel
 class AboutViewModel : ViewModel() {
-    private val _url = MutableStateFlow("https://github.com/Spikeysanju/Expenso")
+    private val _url = MutableStateFlow("")
     val url: StateFlow<String> = _url
 
     fun launchLicense() {
-        _url.value = "https://github.com/Spikeysanju/Expenso/blob/master/LICENSE"
+        _url.value = ""
     }
 
     fun launchRepository() {
-        _url.value = "https://github.com/Spikeysanju/Expenso"
+        _url.value = ""
     }
 }
