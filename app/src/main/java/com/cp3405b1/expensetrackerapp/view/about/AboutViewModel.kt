8dfb781f0c89c6152c4d232e2bce9a5a5feae4f8
Keashyn <68360382+Keashyn@users.noreply.github.com>
2021-08-26 17:34:41 +0800

    edit these files

--- app/src/main/java/com/cp3405b1/expensetrackerapp/view/about/AboutViewModel.kt
+++ app/src/main/java/com/cp3405b1/expensetrackerapp/view/about/AboutViewModel.kt
@@ -7,14 +7,14 @@ import kotlinx.coroutines.flow.StateFlow
 import javax.inject.Inject
 @HiltViewModel
 class AboutViewModel @Inject constructor() : ViewModel() {
-    private val _url = MutableStateFlow("https://github.com/Spikeysanju/Expenso")
+    private val _url = MutableStateFlow("https://github.com/id8lab/scrum-project-assignment-1-and-assignment-2-group_b1-sp52/tree/main/app/src/main/java/com/cp3405b1/expensetrackerapp")
     val url: StateFlow<String> = _url
 
     fun launchLicense() {
-        _url.value = "https://github.com/Spikeysanju/Expenso/blob/master/LICENSE"
+        _url.value = "https://github.com/id8lab/scrum-project-assignment-1-and-assignment-2-group_b1-sp52/tree/main/app/src/main/java/com/cp3405b1/expensetrackerapp"
     }
 
     fun launchRepository() {
-        _url.value = "https://github.com/Spikeysanju/Expenso"
+        _url.value = "https://github.com/id8lab/scrum-project-assignment-1-and-assignment-2-group_b1-sp52/tree/main/app/src/main/java/com/cp3405b1/expensetrackerapp"
     }
 }
