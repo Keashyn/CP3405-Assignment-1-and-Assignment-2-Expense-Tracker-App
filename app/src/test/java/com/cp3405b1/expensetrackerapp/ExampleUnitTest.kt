7b379c5f65bcf49784e7cb2e8538e2a1f90e0b61
ClementSeah <clementhanliang.seah@my.jcu.edu.au>
2021-09-02 17:34:38 +0800

    Fixed ExampleUnitTest.kt error

--- app/src/test/java/com/cp3405b1/expensetrackerapp/ExampleUnitTest.kt
+++ app/src/test/java/com/cp3405b1/expensetrackerapp/ExampleUnitTest.kt
@@ -1,17 +1,17 @@
-package com.cp3405b1.expensetrackerapp;
+package com.cp3405b1.expensetrackerapp
 
-import org.junit.Test;
+import org.junit.Test
 
-import org.junit.Assert.*;
+import org.junit.Assert.*
 
 /**
- * Example local unit test, which will execute on the development machine (host).
+ * Example local unit test, which will execute on the development machine (host)
  *
- * @see <a href="http://d.android.com/tools/testing"> Testing documentation</a>
+ * href="http://d.android.com/tools/testing
  */
-public class ExampleUnitTest {
+class ExampleUnitTest {
     @Test
-    public void addition_isCorrect() {
-        assertEquals(4, 2 + 2);
+    fun addition_isCorrect() {
+        assertEquals(4, 2 + 2)
     }
 }
